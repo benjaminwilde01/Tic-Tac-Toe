f3ed5bad3c952bc29ff42ddfdf05ebd8c19259ff
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-07-22 12:00:11 -0400

    Able to add X to squares on click

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const main = document.querySelector('.main')
 
+const xCount = []
+const oCount = []
 
-const square = document.getElementsByClassName('square');
+const square = document.querySelectorAll('.square');
 console.log(square)
 
+// const test = () => {
+//     console.log("you clicked me!")
+// }
 
-
-
-const test = () => {
-    console.log("you clicked me!")
+const oneTurn = (ev) => {
+    console.log(ev)
+    if (ev.currentTarget.innerText === "" && xCount.length === oCount.length) {
+        ev.currentTarget.innerText = "X"
+    }
+    else if (xCount.length === oCount.length) {
+        ev.currentTarget.innerText = "X"
+        xCount.push(ev)
+        console.log(xCount)
+    }
 }
 
 for (i = 0; i < square.length; i++) {
-    square[i].addEventListener('click', test)
+    square[i].addEventListener('click', oneTurn)
 }
